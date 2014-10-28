24215e099b95a50396d3f9761dcbbbe7535aafcf
StanislavRadkov <Stanislav.vr@gmail.com>
2014-10-28 21:37:33 +0200

    Initial commit

---
+++ LINQExtensions/General.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExtensions
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Replaces a null reference with an empty collection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The collection reference.</param>
+        /// <returns>The collection or an empty one</returns>
+        public static IEnumerable<T> NullToEmpty<T>(this IEnumerable<T> source)
+        {
+            return source ?? Enumerable.Empty<T>();
+        }
+
+        /// <summary>
+        /// Checks if the given collection contains any items. Will not throw an exception if the source is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The collection reference.</param>
+        /// <returns>True if the collection is empty, false otherwise.</returns>
+        public static bool SafeAny<T>(this IEnumerable<T> source)
+        {
+            return source.NullToEmpty().Any();
+        }
+
+        /// <summary>
+        /// Checks if the given collection contains any items that satisfies the given condition. Will not throw an exception if the source is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The collection reference.</param>
+        /// <param name="predicate">The condition.</param>
+        /// <returns>True if the collection is empty, false otherwise.</returns>
+        public static bool SafeAny<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            return source.NullToEmpty().Any(predicate);
+        }
+
+
+        /// <summary>
+        /// Checks if the given collection is empty. Will not throw an exception if the source is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The collection reference.</param>
+        /// <returns>True if the collection is empty, false otherwise.</returns>
+        public static bool SafeNone<T>(this IEnumerable<T> source)
+        {
+            return !source.NullToEmpty().Any();
+        }
+
+        /// <summary>
+        /// Checks if the given collection contains 0 items that satisfies the given condition. Will not throw an exception if the source is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The collection reference.</param>
+        /// <param name="predicate">The condition.</param>
+        /// <returns>True if the collection is empty, false otherwise.</returns>
+        public static bool SafeNone<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            return !source.NullToEmpty().Any(predicate);
+        }
+
+
+        /// <summary>
+        /// Determines whether a collection implementing the IEnumerable interface is null or empty.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The collection reference.</param>
+        /// <returns>
+        ///   <c>true</c> if the collection is null or empty otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source == null || !source.Any();
+        }
+
+        /// <summary>
+        /// Checks if the given collection is empty.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The collection reference.</param>
+        /// <returns>True if the collection is empty, false otherwise.</returns>
+        public static bool None<T>(this IEnumerable<T> source)
+        {
+            return !source.Any();
+        }
+
+        /// <summary>
+        /// Checks if the given collection contains 0 items that satisfies the given condition.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The collection reference.</param>
+        /// <param name="predicate">The condition.</param>
+        /// <returns>True if the collection is empty, false otherwise.</returns>
+        public static bool None<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            return !source.Any(predicate);
+        }
+    }
+}
---
+++ LINQExtensions/Hierarchical.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExtensions
+{
+    public static class Hierarchical
+    {
+        /// <summary>
+        /// Traverses the hierarchy. If the condition is true the subtree for this node will NOT be traversed, and the action
+        /// will be executed only on the node.
+        /// </summary>
+        /// <param name="data">The list of nodes.</param>
+        /// <param name="returnChildren"> Expression to access the descendents of the node.</param>
+        /// <param name="condition">The condition on which the action will be executed.</param>
+        /// <param name="action">The action which will be executed.</param>
+        /// <exception cref="System.ArgumentException">features parameter cannot be null!</exception>
+        public static void TraverseHierarchy<T>(
+            this IEnumerable<T> data,
+            Func<T, IEnumerable<T>> returnChildren,
+            Func<T, bool> condition,
+            Action<T> action)
+        {
+            if (data == null)
+            {
+                throw new ArgumentException("data parameter cannot be null!");
+            }
+
+            if (condition == null)
+            {
+                throw new ArgumentException("condition parameter cannot be null!");
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentException("action parameter cannot be null!");
+            }
+
+            foreach (var e in data)
+            {
+                if (condition(e))
+                {
+                    action(e);
+                }
+                else
+                {
+                    TraverseHierarchy(returnChildren(e), returnChildren, condition, action);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Traverses the hierarchy and executes the action for each node.
+        /// </summary>
+        /// <param name="data">The list of nodes.</param>
+        /// <param name="returnChildren"> Expression to access the descendents of the node.</param>
+        /// <param name="action">The action which will be executed.</param>
+        /// <exception cref="System.ArgumentException">features parameter cannot be null!</exception>
+        public static void TraverseHierarchy<T>(
+            this IEnumerable<T> data,
+            Func<T, IEnumerable<T>> returnChildren,
+            Action<T> action)
+        {
+            if (data == null)
+            {
+                throw new ArgumentException("data parameter cannot be null!");
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentException("action parameter cannot be null!");
+            }
+
+            foreach (var e in data)
+            {
+                action(e);
+
+                TraverseHierarchy(returnChildren(e), returnChildren, action);
+            }
+        }
+    }
+}
