d8993fd1208e045f0995a6ed60945fb1d9a94cc2
StanislavRadkov <Stanislav.vr@gmail.com>
2014-10-30 11:37:40 +0200

    Changed project type from VS2013 to VS2012

---
+++ Tests/General/NullOrEmpty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using LINQExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.General
+{
+    [TestClass]
+    public class NullOrEmpty
+    {
+        [TestMethod]
+        public void TestNullSource()
+        {
+            int[] array = null;
+
+            Assert.AreNotEqual(array.NullToEmpty(), null);
+        }
+
+        [TestMethod]
+        public void TestInitializedSource()
+        {
+            var array = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            Assert.AreEqual(array.NullToEmpty().Count(x => x % 2 == 0), 5);
+        }
+    }
+}
